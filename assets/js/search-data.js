09a8d478091dd15b3f2ce2ce9ddb05120d414a44
shanmathibalan <shanmathibalan@users.noreply.github.com>
2025-10-22 12:39:56 +0000

    Deploying to gh-pages from @ shanmathibalan/shanmathibalan.github.io@8d7e1322e8f053656f3f8a012ed0dc8293fa8adf 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,104 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/assets/pdf/Shanmathi-Gnanabalan.pdf";
+          },
+        },{id: "projects-parton-showers-on-gpus-with-gaps",
+          title: 'Parton Showers on GPUs with GAPS',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/gpu-parton-shower-gaps/";
+            },},{id: "projects-nll-accurate-showers-in-herwig",
+          title: 'NLL Accurate Showers in Herwig',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/nll-parton-shower-herwig/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%73%69%64%64%68%61%72%74%68%73%75%6C%65@%6F%75%74%6C%6F%6F%6B.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/siddharthsule", "_blank");
+        },
+      },{
+        id: 'social-gitlab',
+        title: 'GitLab',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://gitlab.com/siddharthsule", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/2768399", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/sid_physics", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/siddharth-sule", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
